BenzoRom/build

build: add switch to use system ccache
* ccache gets updated semi-frequently and some builders would like
  to be able to take advantage of new features without having to
  rely on the prebuilt version getting updated

Change-Id: I8988c8a25fab3694d84633f957c2b92ce84cf69e
Signed-off-by: mydongistiny <jaysonedson@gmail.com>

core/ccache.mk
@@ -36,8 +36,12 @@ ifneq ($(filter-out false,$(USE_CCACHE)),)
   # See http://petereisentraut.blogspot.com/2011/09/ccache-and-clang-part-2.html
   CCACHE_CPP2 := true
 
-  CCACHE_HOST_TAG := $(HOST_PREBUILT_TAG)
-  ccache := prebuilts/build-tools/$(CCACHE_HOST_TAG)/bin/ccache
+  ifeq ($(USE_SYSTEM_CCACHE),)
+    CCACHE_HOST_TAG := $(HOST_PREBUILT_TAG)
+    ccache := prebuilts/build-tools/$(CCACHE_HOST_TAG)/bin/ccache
+  else
+    ccache := $(shell which ccache)
+  endif
   # Check that the executable is here.
   ccache := $(strip $(wildcard $(ccache)))
   ifdef ccache

dragonroad99/anci_build

Signed-off-by: Reza Adi Pangestu <rezaadipangestu5@gmail.com>

core/ccache.mk
@@ -36,8 +36,12 @@ ifneq ($(filter-out false,$(USE_CCACHE)),)
   # See http://petereisentraut.blogspot.com/2011/09/ccache-and-clang-part-2.html
   CCACHE_CPP2 := true
 
-  CCACHE_HOST_TAG := $(HOST_PREBUILT_TAG)
-  ccache := prebuilts/tools-extras/$(CCACHE_HOST_TAG)/bin/ccache
+  ifeq ($(USE_SYSTEM_CCACHE),)
+    CCACHE_HOST_TAG := $(HOST_PREBUILT_TAG)
+    ccache := prebuilts/tools-ancient/$(CCACHE_HOST_TAG)/bin/ccache
+  else
+    ccache := $(shell which ccache)
+  endif
   # Check that the executable is here.
   ccache := $(strip $(wildcard $(ccache)))
   ifdef ccache